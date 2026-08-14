454f5594f2b239581ca155d88222605d4ce71821
authornjk <authornjk@gmail.com>
2026-08-14 12:51:22 -0600

    v20260814w

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // v20260814v
-const CACHE = 'soiree-prizes-20260814v';
+const CACHE = 'soiree-prizes-20260814w
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(ks => Promise.all(ks.map(k => caches.delete(k)))).then(() => self.clients.claim())
