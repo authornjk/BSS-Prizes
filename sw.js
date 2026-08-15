3882b0c3f2b8df3b757d3caa3f8b21b84ca2432b
authornjk <authornjk@gmail.com>
2026-08-14 23:00:12 -0600

    Tags tab: add Bookmarks/Clothing tags/Misc Items print preview buttons

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// v20260815s
-const CACHE = 'soiree-prizes-20260815s';
+// v20260815t
+const CACHE = 'soiree-prizes-20260815t';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(ks => Promise.all(ks.map(k => caches.delete(k)))).then(() => self.clients.claim())
