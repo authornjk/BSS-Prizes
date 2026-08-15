710f88adcb9d945828891f7c65259b9f99470692
authornjk <authornjk@gmail.com>
2026-08-15 15:03:39 -0600

    Add Prize: opening Category picker automatically right after the last staged field (e.g. Description) advances

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// v20260816i
-const CACHE = 'soiree-prizes-20260816i';
+// v20260816j
+const CACHE = 'soiree-prizes-20260816j';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(ks => Promise.all(ks.map(k => caches.delete(k)))).then(() => self.clients.claim())
