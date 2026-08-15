84ce7d85345372d285383f5638d6b10f6eede533
authornjk <authornjk@gmail.com>
2026-08-15 14:01:03 -0600

    Add prize: one-level-at-a-time staging for Clothing (type->size), Bookish item (type->desc), Jewelry/Misc (description), Enter-to-advance with autofocus, converging at Category

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// v20260816e
-const CACHE = 'soiree-prizes-20260816e';
+// v20260816f
+const CACHE = 'soiree-prizes-20260816f';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(ks => Promise.all(ks.map(k => caches.delete(k)))).then(() => self.clients.claim())
