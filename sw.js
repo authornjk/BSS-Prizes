4e6a08b4a14aa07dd64fd5a95c600b7db0294fdf
authornjk <authornjk@gmail.com>
2026-08-14 20:50:32 -0600

    Goal cards: smaller/shorter chips, equal-weight have/goal numbers, bigger bold needed count, icon-only edit

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// v20260815p
-const CACHE = 'soiree-prizes-20260815p';
+// v20260815q
+const CACHE = 'soiree-prizes-20260815q';
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(
   caches.keys().then(ks => Promise.all(ks.map(k => caches.delete(k)))).then(() => self.clients.claim())
